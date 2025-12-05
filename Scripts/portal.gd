2e8cc230337991b1150374fa55b42c8baa9de37d
pedrigous <pedroig.sousa@gmail.com>
2025-12-05 11:54:02 -0300

    FEAT
    Adicionado portal

---
+++ Scripts/portal.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@export var origem: String
+@export var destino: String
+@export var distancia: int
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		var portais = get_tree().get_nodes_in_group("portais")
+		
+		for portal in portais:
+			if portal.get("origem"):
+				if portal.origem == destino:
+					var x = portal.position.x + portal.distancia
+					var y = portal.position.y
+					
+					body.position = Vector2(x,y)
+	
